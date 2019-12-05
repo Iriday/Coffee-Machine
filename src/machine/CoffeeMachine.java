313325406ca526bc48721bf067de284d833eaf47
Igor <Iriday@mail.ru>
2019-12-05 13:49:43 +0200

    Code cleanup

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -8,23 +8,10 @@ public class CoffeeMachine {
     private static int amountOfBeans = 120;
     private static int amountOfCups = 9;
     private static int amountOfMoney = 550;
-    // private static int numOfCups = 0;
     private static final Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        /*System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");*/
 
-        //System.out.println("Write how many cups of coffee you will need:");
-        //numOfCups = scn.nextInt();
-        // checkIfEnoughIngredientsToMakeSpecifiedAmountOfCups(numOfCups);
-
-        // countIngredients(scn.nextInt());
 
         while (true) {
             String action = askAction();
@@ -40,39 +27,6 @@ public class CoffeeMachine {
                 "%d of disposable cups\n" +
                 "$%d of money\n", amountOfWater, amountOfMilk, amountOfBeans, amountOfCups, amountOfMoney);
     }
-
-   /* public static void checkIfEnoughIngredientsToMakeSpecifiedAmountOfCups(int cups) {
-        int water = amountOfWater;
-        int milk = amountOfMilk;
-        int beans = amountOfBeans;
-        int maxCups = 0;
-
-        // check how many cups machine can make
-        for (; true; ) {
-            if (water >= waterOneCup && milk >= milkOneCup && beans >= beansOneCup) {
-                water -= waterOneCup;
-                milk -= milkOneCup;
-                beans -= beansOneCup;
-                maxCups++;
-            } else {
-                break;
-            }
-        }
-        if (maxCups == cups) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (maxCups > cups) {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n", maxCups - cups);
-        } else {
-            System.out.printf("No, I can only make %d cup(s) of coffee\n", maxCups);
-        }
-    }*/
-
-    /*public static void countIngredients(int cups) {
-        System.out.printf("For %d cups of coffee you will need:\n", cups);
-        System.out.println(waterForOneCup * cups + " ml of water");
-        System.out.println(milkForOneCup * cups + " ml of milk");
-        System.out.println(beansForOneCup * cups + " g of coffee beans");
-    }*/
 
     private static String askAction() {
         while (true) {
@@ -133,7 +87,15 @@ public class CoffeeMachine {
         amountOfBeans -= beverage.getBeans();
         amountOfCups--;
         amountOfMoney += beverage.getPrice();
-        System.out.println("I have enough resources, making you a coffee!");
+        System.out.println("I have enough resources, starting to make a coffee!");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        if (beverage.getMilk() != 0) {
+            System.out.println("Pouring some milk into the cup");
+        }
+        System.out.println("Coffee is ready!");
     }
 
     private static void sellBeverage() {
