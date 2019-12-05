17fa49905d3852f765f7e4cf4bf9fbee668ee4ca
Igor <Iriday@mail.ru>
2019-12-05 14:49:29 +0200

    Reworked version with states

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -3,23 +3,72 @@ package machine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    private static int amountOfWater = 400;
-    private static int amountOfMilk = 540;
-    private static int amountOfBeans = 120;
-    private static int amountOfCups = 9;
-    private static int amountOfMoney = 550;
-    private static final Scanner scn = new Scanner(System.in);
+    private int amountOfWater = 400;
+    private int amountOfMilk = 540;
+    private int amountOfBeans = 120;
+    private int amountOfCups = 9;
+    private int amountOfMoney = 550;
+    private States state = States.CHOOSING_AN_ACTION;
 
-    public static void main(String[] args) {
+    CoffeeMachine() {
+        System.out.println("Write action (buy, fill, take, remaining, exit):");
+    }
 
+    private enum States {
+        /* SLEEP_MODE(States.InnerStates.SLEEP),*/ FILLING_MACHINE(InnerStates.ADD_WATER), CHOOSING_AN_ACTION(InnerStates.STANDARD), CHOOSING_A_VARIANT_OF_COFFEE(InnerStates.STANDARD);
 
-        while (true) {
-            String action = askAction();
-            doAction(action);
+        private enum InnerStates {
+            //  SLEEP,
+            STANDARD,
+            //fill
+            ADD_WATER, ADD_MILK, ADD_BEANS, ADD_CUPS,
+            // choose action
+            //BUY, FILL, TAKE, REMAINING, EXIT,
+            // choose variant of coffee
+            //ESPRESSO, LATTE, CAPPUCCINO, BACK;
+        }
+
+        InnerStates innerState;
+
+        States(InnerStates innerState) {
+            this.innerState = innerState;
+        }
+
+        States() {
         }
     }
 
-    private static void printStatus() {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+        while (true) {
+            coffeeMachine.instruction(scn.nextLine());
+        }
+    }
+
+    public void instruction(String instruction) {
+        switch (state) {
+            case FILLING_MACHINE:
+                fillMachine(instruction);
+                break;
+            case CHOOSING_AN_ACTION:
+                doAction(instruction);
+                if (state == States.CHOOSING_AN_ACTION) {
+                    System.out.println("Write action (buy, fill, take, remaining, exit):");
+                }
+                break;
+            case CHOOSING_A_VARIANT_OF_COFFEE:
+                sellBeverage(instruction);
+                if (state == States.CHOOSING_AN_ACTION) {
+                    System.out.println("Write action (buy, fill, take, remaining, exit):");
+                }
+                break;
+        }
+    }
+
+
+    private void printStatus() {
         System.out.println("The coffee machine has:");
         System.out.printf("%d of water\n" +
                 "%d of milk\n" +
@@ -28,10 +77,10 @@ public class CoffeeMachine {
                 "$%d of money\n", amountOfWater, amountOfMilk, amountOfBeans, amountOfCups, amountOfMoney);
     }
 
-    private static String askAction() {
+   /* private static String askAction() {
         while (true) {
             System.out.println("Write action (buy, fill, take, remaining, exit):");
-            String chosenAction = scn.next();
+            //String chosenAction = scn.next();
             if (chosenAction.equalsIgnoreCase("buy")
                     || chosenAction.equalsIgnoreCase("fill")
                     || chosenAction.equalsIgnoreCase("take")
@@ -42,15 +91,18 @@ public class CoffeeMachine {
                 System.out.println("Not a valid action, try again.");
             }
         }
-    }
+    }*/
 
-    private static void doAction(String action) {
+    private void doAction(String action) {
         switch (action) {
             case "buy":
-                sellBeverage();
+                state = States.CHOOSING_A_VARIANT_OF_COFFEE;
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
                 break;
             case "fill":
-                fillMachine();
+                state = States.FILLING_MACHINE;
+                state.innerState = States.InnerStates.ADD_WATER;
+                System.out.println("Write how many ml of water do you want to add:");
                 break;
             case "take":
                 giveMoney();
@@ -63,7 +115,7 @@ public class CoffeeMachine {
         }
     }
 
-    private static boolean checkIfEnoughIngredients(Beverages beverage) {
+    private boolean checkIfEnoughIngredients(Beverages beverage) {
         if (amountOfWater - beverage.getWater() < 0) {
             System.out.println("Sorry, not enough water!");
             return false;
@@ -81,7 +133,7 @@ public class CoffeeMachine {
         }
     }
 
-    private static void makeBeverage(Beverages beverage) {
+    private void makeBeverage(Beverages beverage) {
         amountOfWater -= beverage.getWater();
         amountOfMilk -= beverage.getMilk();
         amountOfBeans -= beverage.getBeans();
@@ -98,43 +150,57 @@ public class CoffeeMachine {
         System.out.println("Coffee is ready!");
     }
 
-    private static void sellBeverage() {
+    private void sellBeverage(String choice) {
         Beverages beverage;
-        while (true) {
-            System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-            String choice = scn.next();
-            if (choice.equalsIgnoreCase("back")) {
-                return;
-            }
-            beverage = Beverages.getBeverage(choice);
-            if (beverage == null) {
-                System.out.println("Wrong answer, try again.");
-                continue;
-            } else {
-                break;
-            }
+        // while (true) {
+
+        if (choice.equalsIgnoreCase("back")) {
+            state = States.CHOOSING_AN_ACTION;
+            return;
         }
+        beverage = Beverages.getBeverage(choice);
+        if (beverage == null) {
+            System.out.println("Wrong answer, try again.");
+            // continue;
+        } // else {
+//                break;
+//            }
+        //}
         boolean enough = checkIfEnoughIngredients(beverage);
         if (enough) {
             makeBeverage(beverage);
-        } else {
-            return;
         }
-
+        state = States.CHOOSING_AN_ACTION;
     }
 
-    private static void fillMachine() {
-        System.out.println("Write how many ml of water do you want to add:");
-        amountOfWater += scn.nextInt();
-        System.out.println("Write how many ml of milk do you want to add:");
-        amountOfMilk += scn.nextInt();
-        System.out.println("Write how many grams of coffee beans do you want to add:");
-        amountOfBeans += scn.nextInt();
-        System.out.println("Write how many cups of coffee do you want add:");
-        amountOfCups += scn.nextInt();
+    private void fillMachine(String instruction) {
+
+        switch (state.innerState) {
+            case ADD_WATER:
+                amountOfWater += Integer.parseInt(instruction);
+                state.innerState = States.InnerStates.ADD_MILK;
+                System.out.println("Write how many ml of milk do you want to add:");
+                break;
+            case ADD_MILK:
+                amountOfMilk += Integer.parseInt(instruction);
+                state.innerState = States.InnerStates.ADD_BEANS;
+                System.out.println("Write how many grams of coffee beans do you want to add:");
+                break;
+            case ADD_BEANS:
+                amountOfBeans += Integer.parseInt(instruction);
+                state.innerState = States.InnerStates.ADD_CUPS;
+                System.out.println("Write how many cups of coffee do you want add:");
+                break;
+            case ADD_CUPS:
+                amountOfCups += Integer.parseInt(instruction);
+                state = States.CHOOSING_AN_ACTION;
+                //state.innerState = States.InnerStates.ADD_WATER;
+                System.out.println("Write action (buy, fill, take, remaining, exit):");
+                break;
+        }
     }
 
-    private static void giveMoney() {
+    private void giveMoney() {
         System.out.printf("I gave you $%d\n", amountOfMoney);
         amountOfMoney = 0;
     }
